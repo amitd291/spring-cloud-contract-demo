5b0e2e617060c6e492382c5ee0f0cb52644c3532
Amit Dash <amitdash291@gmail.com>
2024-10-31 18:28:45 +0530

    Fix contract tests

--- Producer/src/contractTest/kotlin/com/dash/springcloudcontractdemo/contracts/ContractVerifierBase.kt
+++ Producer/src/contractTest/kotlin/com/dash/springcloudcontractdemo/contracts/ContractVerifierBase.kt
@@ -3,10 +3,18 @@ package com.dash.springcloudcontractdemo.contracts
 import com.dash.springcloudcontractdemo.api.OrdersController
 import io.restassured.module.mockmvc.RestAssuredMockMvc
 import org.junit.jupiter.api.BeforeEach
+import org.mockito.InjectMocks
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest
 open class ContractVerifierBase {
+
+    @Autowired
+    lateinit var ordersController: OrdersController
+
     @BeforeEach
     fun setup() {
-        RestAssuredMockMvc.standaloneSetup(OrdersController())
+        RestAssuredMockMvc.standaloneSetup(ordersController)
     }
 }
